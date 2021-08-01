c43731a5fe6cf3d09ebc30fa265e0692fe6d321d
karlnicholas <karl.nicholas@outlook.com>
2021-08-01 11:44:59 -0400

    Cleanup

--- src/main/java/com/example/webfluxplay/api/RoutingConfig.java
+++ src/main/java/com/example/webfluxplay/api/RoutingConfig.java
@@ -1,10 +1,7 @@
 package com.example.webfluxplay.api;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
-import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
